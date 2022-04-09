b89caff2e61021fe66bdcb9a3f8d32aeb5a29def
MurselCiftci <mursel_ciftci@outlook.com>
2022-04-09 18:19:49 +0200

    1. Convert list of pets to char array in separate letters of rand pet 2. Add check whether input letter of user exists in random pet char array

--- Hangman/Program.cs
+++ Hangman/Program.cs
@@ -11,17 +11,26 @@ namespace Hangman
             // Pick a random pet in the list of pets below 
             Random rand = new Random();
             string[] petList = { "CAT", "DOG", "SNAKE" };
-            Console.WriteLine(petList[rand.Next(0, petList.Length)]);
+            string randomPet = petList[rand.Next(petList.Length)];
+
+            var randomPetSeparateLetters = randomPet.ToCharArray();
+            
+            /*
+            for (int i = 0; i < randomPet.Length; i++)
+                Console.WriteLine(randomPet[i]);
+            */
 
             // Ask the user to fill in a character
             Console.WriteLine("Insert a character: ");
             char inputLetter = Console.ReadLine()[0];
             string inputLetterToStr = inputLetter.ToString();
-            string upperCaseLetter = inputLetterToStr.ToUpper();
-            Console.WriteLine(upperCaseLetter);
-
-            /* Check whether the random picked pet has a character that the user filled in
-            bool inputCharIsPartOfRandPet = randomPet.Contains(upperCaseLetter); */
+            string inputUpperCaseLetter = inputLetterToStr.ToUpper();
+            
+            bool letterFoundInRandomWord = Array.Exists(randomPetSeparateLetters, element => element.Equals(inputUpperCaseLetter));
+            /*
+            bool inputCharIsPartOfRandPet = randomPetSeparateLetters.Contains(upperCaseLetter); 
+            */       
+            
         }
     }
 }
