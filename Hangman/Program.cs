01476aaa0ab61cce600741f189ee4e51c23784f4
MurselCiftci <mursel_ciftci@outlook.com>
2022-04-09 14:10:42 +0200

    Simplified random pet selection.

--- Hangman/Program.cs
+++ Hangman/Program.cs
@@ -8,22 +8,20 @@ namespace Hangman
     {
         static void Main(string[] args)
         {
-            var listOfPets = new List<string>
-            {
-                "DOG",
-                "CAT",
-                "SNAKE"
-            };
+            // Pick a random pet in the list of pets below 
             Random rand = new Random();
-            var randomPet = rand.Next(listOfPets.Count);
-            Console.WriteLine(listOfPets[randomPet]);
+            string[] petList = { "CAT", "DOG", "SNAKE" };
+            Console.WriteLine(petList[rand.Next(0, petList.Length)]);
 
+            // Ask the user to fill in a character
             Console.WriteLine("Insert a character: ");
             char inputLetter = Console.ReadLine()[0];
             string inputLetterToStr = inputLetter.ToString();
             string upperCaseLetter = inputLetterToStr.ToUpper();
             Console.WriteLine(upperCaseLetter);
 
+            /* Check whether the random picked pet has a character that the user filled in
+            bool inputCharIsPartOfRandPet = randomPet.Contains(upperCaseLetter); */
         }
     }
 }
