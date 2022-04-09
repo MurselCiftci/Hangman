0e232f56a3f041688846b9ac4461bf17ca30a511
MurselCiftci <mursel_ciftci@outlook.com>
2022-04-09 13:10:49 +0200

    Changes
    1. Added section to enter a string and added functionality to convert to uppercase.
    2. Changed pet names to all uppercase.

--- Hangman/Program.cs
+++ Hangman/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Hangman
 {
@@ -6,7 +8,22 @@ namespace Hangman
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var listOfPets = new List<string>
+            {
+                "DOG",
+                "CAT",
+                "SNAKE"
+            };
+            Random rand = new Random();
+            var randomPet = rand.Next(listOfPets.Count);
+            Console.WriteLine(listOfPets[randomPet]);
+
+            Console.WriteLine("Insert a character: ");
+            char inputLetter = Console.ReadLine()[0];
+            string inputLetterToStr = inputLetter.ToString();
+            string upperCaseLetter = inputLetterToStr.ToUpper();
+            Console.WriteLine(upperCaseLetter);
+
         }
     }
 }
