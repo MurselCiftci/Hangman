06d25d76142e1205451af040c614f5be738b56b5
MurselCiftci <mursel_ciftci@outlook.com>
2022-04-10 17:03:03 +0200

    1. Changed the pet list

--- Hangman/Program.cs
+++ Hangman/Program.cs
@@ -10,27 +10,39 @@ namespace Hangman
         {
             // Pick a random pet in the list of pets below 
             Random rand = new Random();
-            string[] petList = { "CAT", "DOG", "SNAKE" };
-            string randomPet = petList[rand.Next(petList.Length)];
-
-            var randomPetSeparateLetters = randomPet.ToCharArray();
             
-            /*
-            for (int i = 0; i < randomPet.Length; i++)
-                Console.WriteLine(randomPet[i]);
-            */
+            // Create a list of Pets
+            List<string> petList = new List<string>()
+            {
+                "CAT",
+                "DOG",
+                "SNAKE",
+                "GIRAFFE"
+            };
+            // Select a random pet from the listPet
+            int randPetListIndex = rand.Next(petList.Count);
 
             // Ask the user to fill in a character
             Console.WriteLine("Insert a character: ");
             char inputLetter = Console.ReadLine()[0];
             string inputLetterToStr = inputLetter.ToString();
             string inputUpperCaseLetter = inputLetterToStr.ToUpper();
-            
-            bool letterFoundInRandomWord = Array.Exists(randomPetSeparateLetters, element => element.Equals(inputUpperCaseLetter));
+
             /*
-            bool inputCharIsPartOfRandPet = randomPetSeparateLetters.Contains(upperCaseLetter); 
-            */       
+            bool found = false;
+            for (int i = 0; i < randomPetSeparateLetters.Length; i++)
+            {
+                if (inputUpperCaseLetter == randomPetSeparateLettersToStr)
+                {
+
+                    found = true;
+                }
+            }
+            */
             
+
+           
+
         }
     }
 }
