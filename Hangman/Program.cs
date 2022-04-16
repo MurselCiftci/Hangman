d45870602c6dcb32e17e31be2fbaa9ff85474941
MurselCiftci <mursel_ciftci@outlook.com>
2022-04-16 17:01:26 +0200

    Changes. 1. Wrapped game in while loop 2. Added gameRunning variable 3. Added else statement if inputletter doesn't exist in secret word

--- Hangman/Program.cs
+++ Hangman/Program.cs
@@ -8,9 +8,9 @@ namespace Hangman
     {
         static void Main(string[] args)
         {
-            // Pick a random pet in the list of pets below 
+            // Initialize randomizer 
             Random rand = new Random();
-            
+
             // Create a list of Pets
             List<string> petList = new List<string>()
             {
@@ -19,59 +19,57 @@ namespace Hangman
                 "SNAKE",
                 "GIRAFFE"
             };
- 
-            // Take random pet as a string from a string list
-            string randomPetFromPetList = petList[rand.Next(petList.Count)];
-            
-            // Split the random pet word into characters of each letter
-            List<char> petLetters = new List<char>();
-            petLetters.AddRange(randomPetFromPetList);
 
-            // Creates a list of underscores for each letter of the random pet
-            List<char> hangmanSecretLetterUnderscores = new List<char>();
-            char underscore = '_';
-            
-            for (int i = 0; i < petLetters.Count; i++)
-            {
-                hangmanSecretLetterUnderscores.Add(underscore);
-            }
-            
-            // Console.WriteLine(string.Join(" ", hangmanSecretLetterUnderscores));
-            
-            // Ask the user to fill in a character
-            Console.WriteLine("Insert a character: ");
-            char inputLetter = Console.ReadLine()[0];
-            Console.Clear();
+            bool gameRunning = true;
 
-            // bool found = false;
-            // Checks whether inputletter matches a letter from random pet, if so it replaces underscore in right position for letter
-            for (int i = 0; i < randomPetFromPetList.Length;i++)
+            while (gameRunning == true)
             {
-                if (inputLetter == petLetters[i])
+                // In section below initialize a new game
+                // Take random pet as a string from a string list
+                string randomPetFromPetList = petList[rand.Next(petList.Count)];
+
+                // Split the random pet word into characters of each letter
+                List<char> petLetters = new List<char>();
+                petLetters.AddRange(randomPetFromPetList);
+
+                // Creates a list of underscores for each letter of the random pet
+                List<char> hangmanSecretLetterUnderscores = new List<char>();
+                char underscore = '_';
+
+                for (int i = 0; i < petLetters.Count; i++)
                 {
-                    Console.Clear();
-                    Console.WriteLine($"Good job! The letter {inputLetter} is in the secret word!");
-                    hangmanSecretLetterUnderscores.RemoveAt(i);
-                    hangmanSecretLetterUnderscores.Insert(i, inputLetter);
-                    Console.WriteLine(string.Join(" ", hangmanSecretLetterUnderscores));
-                    // found = true;
+                    hangmanSecretLetterUnderscores.Add(underscore);
                 }
 
+                while (true)
+                {
+                    // Ask the user to fill in a character
+                    Console.WriteLine("Insert a character: ");
+                    char inputLetter = Console.ReadLine()[0];
+                    Console.Clear();
+
+                    // bool found = false;
+                    // Checks whether inputletter matches a letter from random pet, if so it replaces underscore in right position for letter
+                    for (int i = 0; i < randomPetFromPetList.Length; i++)
+                    {
+                        if (inputLetter == petLetters[i])
+                        {
+                            Console.Clear();
+                            Console.WriteLine($"Good job! The letter {inputLetter} is in the secret word!");
+                            hangmanSecretLetterUnderscores.RemoveAt(i);
+                            hangmanSecretLetterUnderscores.Insert(i, inputLetter);
+                            Console.WriteLine(string.Join(" ", hangmanSecretLetterUnderscores));
+                            // found = true;
+                        }
+                        else
+                        {
+                            Console.WriteLine($"{inputLetter} does not exist in the secret word");
+                        }
+                    }
+                    break;
+                }
+                break;
             }
-
-            //if (inputLetter == petLetters[0])
-            //    found = true;
-
-            //if (inputLetter == petLetters[1])
-            //    found = true;
-
-            //if (inputLetter == petLetters[2])
-            //    found = true;
-
-
-
-
-
         }
     }
 }
